39dbf67cd1ff2d6c61f00a20c866f3d3421e89a0
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2023-08-17 04:21:14 +0530

    categories and products

--- ui/src/app/pages/admin/categories/categories.component.ts
+++ ui/src/app/pages/admin/categories/categories.component.ts
@@ -5,5 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './categories.component.html',
 })
 export class CategoriesComponent {
+  constructor() {}
 
+  categories: any = [];
+  searchTerm = "";
+  filteredCategories: any = [];
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  fetchCategories() {
+    this.categories = [
+      {
+        _id: '1',
+        name: 'Drinks',
+      },
+      {
+        _id: '2',
+        name: 'Food',
+      },
+      {
+        _id: '3',
+        name: 'Merchandise',
+      },
+    ];
+
+    this.filteredCategories = this.categories;
+  }
+
+  performSearch() {
+    if (this.searchTerm) {
+      const lowerCaseSearch = this.searchTerm.toLowerCase();
+      this.filteredCategories = this.categories.filter((category: any) =>
+        category.name.toLowerCase().includes(lowerCaseSearch)
+      );
+    } else {
+      this.filteredCategories = this.categories;
+    }
+  }
 }
