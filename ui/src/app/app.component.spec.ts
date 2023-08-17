c1cfb6c8ce8158184bfb15f724081cb66fcdda85
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2023-08-17 14:04:58 +0530

    minor touches

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ui'`, () => {
+  it(`should have as title 'Coffee Cafe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ui');
+    expect(app.title).toEqual('Coffee Cafe');
   });
 
   it('should render title', () => {
